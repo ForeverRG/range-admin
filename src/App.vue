e5010193cc39685cf5eb38b7d610cbf22a924898
Range Chen(陈浩然) <range.chen@QUECTEL.COM>
2021-10-12 09:40:39 +0800

    build: 项目架构搭建(一)

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,15 @@
   </div>
   <router-view />
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
 
 <style lang="less">
 #app {
